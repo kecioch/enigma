991c63ffcd27c6479f405babfc410e792662dd74
Kevin Cioch <kevin.cioch@gmail.com>
2024-11-14 02:22:32 +0100

    Added sound effect for key press

--- src/components/InputKeys/InputKey.tsx
+++ src/components/InputKeys/InputKey.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 interface Props {
   char: string;
@@ -7,22 +7,62 @@ interface Props {
 const InputKey = ({ char }: Props) => {
   const [isPressed, setIsPressed] = useState(false);
 
-  const handleMouseDown = () => setIsPressed(true);
-  const handleMouseUp = () => setIsPressed(false);
+  const playSoundEffect = () => {
+    // ---------------------------------------------
+    // Sound Effect from https://www.zapsplat.com/
+    // ---------------------------------------------
+    const sound = new Audio("./sounds/typewriter_button_press.mp3");
+    sound.volume = 0.5;
+    sound.play();
+  };
+
+  const handleMouseDown = useCallback(() => {
+    if (!isPressed) {
+      playSoundEffect();
+      setIsPressed(true);
+    }
+  }, [isPressed]);
+
+  const handleKeyUp = () => setIsPressed(false);
+
+  const handleTouchStart = () => {
+    setIsPressed(true);
+  };
 
   const pressedStyle = isPressed
     ? "border-slate-700 text-slate-800"
     : "border-white text-white";
+
+  // Handle keyboard events
+  useEffect(() => {
+    const onKeyDown = (ev: any) => {
+      if (ev.key.toLowerCase() === char.toLowerCase() && !isPressed)
+        handleMouseDown();
+    };
+
+    const onKeyUp = (event: any) => {
+      if (event.key.toLowerCase() === char.toLowerCase()) handleKeyUp();
+    };
+
+    window.addEventListener("keydown", onKeyDown);
+    window.addEventListener("keyup", onKeyUp);
+
+    // Cleanup event listeners on unmount
+    return () => {
+      window.removeEventListener("keydown", onKeyDown);
+      window.removeEventListener("keyup", onKeyUp);
+    };
+  }, [char, isPressed, handleMouseDown]);
 
   return (
     <div className="flex flex-col justify-end items-center">
       <button
         className={`border-[0.1em] rounded-full w-7 h-7 sm:w-10 sm:h-10 flex justify-center items-center ${pressedStyle}`}
         onMouseDown={handleMouseDown}
-        onMouseUp={handleMouseUp}
-        onMouseLeave={handleMouseUp}
-        onTouchStart={handleMouseDown}
-        onTouchEnd={handleMouseUp}
+        onMouseUp={handleKeyUp}
+        onMouseLeave={handleKeyUp}
+        onTouchStart={handleTouchStart}
+        onTouchEnd={handleKeyUp}
       >
         <span className="text-center text-xl">{char}</span>
       </button>
