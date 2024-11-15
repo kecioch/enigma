b705ff54f69f13d6b6549f2b76b87e7cf9657a30
Kevin Cioch <kevin.cioch@gmail.com>
2024-11-15 04:20:59 +0100

    Refactored HeadlineDivider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import EnigmaContainer from "./components/EnigmaContainer";
+import Footer from "./components/Footer/Footer";
+import HeadlineDivider from "./components/UI/HeadlineDivider";
 
 function App() {
   return (
     <div className="flex flex-col items-center gap-8 mt-11 mb-10">
       <div className="flex flex-row justify-center items-center w-full gap-10">
-        <div className="bg-[#0CABA8] h-[0.1em] flex-1" />
+        <HeadlineDivider />
         <h1 className="text-3xl text-white uppercase font-light">Enigma</h1>
-        <div className="bg-[#0CABA8] h-[0.1em] flex-1" />
+        <HeadlineDivider />
       </div>
       <EnigmaContainer />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/UI/HeadlineDivider.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HeadlineDivider = () => {
+  return <div className="bg-[#0CABA8] h-[0.1em] flex-1" />;
+};
+
+export default HeadlineDivider;
