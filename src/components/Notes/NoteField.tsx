c69429e2ff46e99db03b9c4b3aca915c85189579
Kevin Cioch <kevin.cioch@gmail.com>
2024-11-15 04:03:20 +0100

    Fixed mobile paste bug & changed border on textarea focus

--- src/components/Notes/NoteField.tsx
+++ src/components/Notes/NoteField.tsx
@@ -1,4 +1,4 @@
-import React, { ClipboardEvent, FormEvent, KeyboardEvent } from "react";
+import React, { ChangeEvent, ClipboardEvent, useRef } from "react";
 import { faCopy, faTrash } from "@fortawesome/free-solid-svg-icons";
 import ActionButton from "./ActionButton";
 import { useClipboard } from "../../hooks/useClipboard";
@@ -17,30 +17,30 @@ interface Props {
 const NoteField = ({ mode, text, onChange, onPaste, onClear }: Props) => {
   const copyText = useClipboard();
   const isUserInput = mode === "USER_INPUT";
+  const isPasting = useRef(false);
 
   const handleClear = () => onClear && onClear();
 
   const handleCopy = () => copyText(text);
 
   const handlePaste = (ev: ClipboardEvent<HTMLTextAreaElement>) => {
+    isPasting.current = true;
     const text = ev.clipboardData.getData("text");
     onPaste && onPaste(text);
   };
 
-  const handleInput = (ev: FormEvent<HTMLTextAreaElement>) => {
+  const handleChange = (ev: ChangeEvent<HTMLTextAreaElement>) => {
     // Handle Input for textarea only on mobile devices.
     // For web the KeyDown event listeners on the keys are responsible
     if (!isMobile) return;
-    const value = ev.currentTarget.value;
-    const prevValue = ev.currentTarget.dataset.prevValue || ""; // Track previous value
-    ev.currentTarget.dataset.prevValue = value; // Store current value for next input
-
-    // Compare old and new length to disallow backspace on mobile
-    // If the new value length is greater than the old value, it means a new character is added
-    if (value.length > prevValue.length) {
-      const char = value[value.length - 1];
-      if (onChange && char) onChange(char);
+    if (isPasting.current) {
+      isPasting.current = false;
+      return;
     }
+    // alert("INPUT");
+    const value = ev.currentTarget.value;
+    const char = value[value.length - 1];
+    if (onChange && char !== undefined && char.length === 1) onChange(char);
   };
 
   return (
@@ -65,12 +65,12 @@ const NoteField = ({ mode, text, onChange, onPaste, onClear }: Props) => {
         />
       </div>
       <textarea
-        className="w-full mt-3 rounded-lg resize-none p-2 font-mono tracking-wide bg-[#78d6d4] h-[15em] sm:h-[20em]"
+        className="w-full mt-3 rounded-lg resize-none p-2 font-mono tracking-wide bg-[#78d6d4] h-[15em] sm:h-[20em] border-2 border-transparent focus:outline-none focus:border-[#023535]"
         disabled={!isUserInput}
         value={text}
         onPaste={handlePaste}
         spellCheck={false}
-        onInput={handleInput}
+        onChange={handleChange}
       />
     </div>
   );
